24cc11167850137ba73859d56971fe3467966f76
Mark Zieg <mark@zieg.com>
2022-12-16 15:36:55 -0500

    change logic to default high for all InGaAs

--- WasatchVCPPLib/WasatchVCPPLib/Spectrometer.cpp
+++ WasatchVCPPLib/WasatchVCPPLib/Spectrometer.cpp
@@ -130,7 +130,7 @@ WasatchVCPP::Spectrometer::Spectrometer(WPVCPP_UDEV_TYPE* udev, int pid, int ind
     bufSubspectrum.resize(pixelsPerEndpoint * 2);
 
     // default high for Raman, low otherwise
-    setHighGainModeEnable(eeprom.excitationNM > 0)
+    setHighGainModeEnable(isInGaAs());
 
     logger.debug("Spectrometer::ctor: done");
 }
