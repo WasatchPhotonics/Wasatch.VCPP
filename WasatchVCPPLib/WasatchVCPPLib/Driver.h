b83b1db7a40b347e56d135ae96fd2a72ff11acea
Mark Zieg <mzieg@wasatchphotonics.com>
2024-02-22 11:58:38 -0500

    [1.0.21] cutting release

--- WasatchVCPPLib/WasatchVCPPLib/Driver.h
+++ WasatchVCPPLib/WasatchVCPPLib/Driver.h
@@ -63,7 +63,7 @@ namespace WasatchVCPP
             //!
             //! @note this value is checked by scripts/deploy against the given 
             //!       release tag
-            const std::string libraryVersion = "1.0.19";
+            const std::string libraryVersion = "1.0.21";
 
             static Driver* getInstance();
             static void destroy();
